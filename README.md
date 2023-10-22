ba8cd35798a084d3a12f189ac336fe36da1b8ef2
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-10-22 09:07:12 +0530

    Add new files

---
+++ README.md
@@ -0,0 +1,9 @@
+### TEMPERATURE CONVERTER
+
+## Problem That This Software Solves
+This is a temperature conversion software which is designed to convert degrees celsius to fahrenheit or fahrenheit to degrees Celsius
+
+## Programming languages used
+[![HTML](https://img.shields.io/badge/HTML-5-red?style=flat&logo=html5)](https://developer.mozilla.org/en-US/docs/Web/HTML)
+[![CSS](https://img.shields.io/badge/CSS-3-blue?style=flat&logo=css3)](https://developer.mozilla.org/en-US/docs/Web/CSS)
+[![JavaScript](https://img.shields.io/badge/JavaScript-ES6-yellow?style=flat&logo=javascript)](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
---
+++ script.js
@@ -0,0 +1,27 @@
+function convertTemperature() {
+    const inputTemperature = parseFloat(document.getElementById("num").value);
+    const unit = document.getElementById("unit").value;
+
+    let convertedTemperature;
+
+    if (unit === "Celsius") {
+        // Temperature Conversion to Fahrenheit
+        
+        
+        convertedTemperature = (inputTemperature * 9/5) + 32;
+        document.getElementById("result").textContent = `${inputTemperature} degrees Celcius = ${convertedTemperature} degrees Fahrenheit.`;
+    } 
+    
+    else if (unit === 'Fahrenheit') {
+        // Temperature Conversion to Celsius
+       
+       
+        convertedTemperature = (inputTemperature - 32) * 5/9;
+        document.getElementById("result").textContent = `${inputTemperature} degrees Fahrenheit = ${convertedTemperature} degrees Celsius.`;
+    } 
+    
+    
+    else {
+        document.getElementById("result").textContent = 'Please select a valid unit.';
+    }
+}
